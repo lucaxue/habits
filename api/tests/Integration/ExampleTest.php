4052c909b5df0d1864dc3df96a6df8a4bb8248f8
Luca Xue <lxue02@gmail.com>
2021-09-18 15:15:15 +0100

    Add habits api test cases

--- api/tests/Integration/ExampleTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-// uses(RefreshDatabase::class);
-
-test('example', function () {
-    $this->get('/')->assertStatus(200);
-});
---
+++ api/tests/Integration/HabitsApiTest.php
@@ -0,0 +1,19 @@
+<?php
+
+it("retrieves this week's habits", function () {
+});
+
+it('can mark a habit as complete', function () {
+});
+
+it('can mark a habit as missed', function () {
+});
+
+it('automatically marks a habit as missed when a day passes', function () {
+});
+
+it('can plan a new habit', function () {
+});
+
+it('starts a habit on time', function () {
+});
