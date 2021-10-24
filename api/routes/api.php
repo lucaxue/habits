be86159669fa6987e9a985e57aa97c6bb7b18da4
Luca Xue <lxue02@gmail.com>
2021-10-24 15:28:43 +0100

    Add sanctum token route + testing route

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 use HabitTracking\Infrastructure\HabitController;
 
 /*
@@ -14,6 +17,26 @@ use HabitTracking\Infrastructure\HabitController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('{any}', fn (string $any) => $any);
+
+Route::post('sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', fn (Request $request) => $request->user());
