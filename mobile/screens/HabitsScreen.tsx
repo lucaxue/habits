acfcc206802d96604ac38422f7054cf44bfed385
Luca Xue <lxue02@gmail.com>
2021-11-06 17:22:24 +0000

    Update habits screen to fetch actual habits

--- mobile/screens/HabitsScreen.tsx
+++ mobile/screens/HabitsScreen.tsx
@@ -1,71 +1,78 @@
+import axios from 'axios';
 import * as React from 'react';
 import { useState } from 'react';
-import { Button, Switch } from 'react-native';
+import { ScrollView, Switch } from 'react-native';
 import tailwind from 'tailwind-rn';
 
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-const dummyHabits = [
-  {
-    id: 'some-uuid-1',
-    name: 'Read Book',
-    streak: 'P0Y0M0D',
-    completed: false,
-  },
-  {
-    id: 'some-uuid-2',
-    name: 'Learning Arabic',
-    streak: 'P0Y0M0D',
-    completed: false,
-  },
-];
-
-type Streak = { years: number; months: number; days: number };
-
-function convertToStreak(streak: string): Streak {
-  const matches = streak.match(/P(\d+)Y(\d+)M(\d+)D/);
-  if (!matches || matches.length !== 4) {
-    throw new Error(`Invalid streak format: ${streak}`);
-  }
-  const [, years, months, days] = matches;
-  return { years: +years, months: +months, days: +days };
+interface Habit {
+  id: string;
+  name: string;
+  streak: string;
+  completed: boolean;
+  frequency: {
+    type: string;
+    days?: string;
+  };
 }
 
-export default function TrackingScreen({
+export default function HabitsScreen({
   navigation,
 }: RootTabScreenProps<'Habits'>) {
-  const [habits, setHabits] = useState(dummyHabits);
+  const [habits, setHabits] = useState<Habit[]>([]);
 
-  function toggleHabitCompletion(id: string) {
-    let habit = habits.find(h => h.id === id);
-    if (!habit) {
-      throw Error(`Habit of id ${id} not found`);
-    }
-    habit.completed = !habit.completed;
-    setHabits([...habits.filter(h => h.id !== id), habit]);
+  React.useEffect(() => {
+    (async function getHabits() {
+      const { data } = await axios.get<Habit[]>('api/habits/today');
+      setHabits(data);
+    })();
+  }, []);
+
+  async function completeHabit(id: string) {
+    const { data } = await axios.put<Habit>(`api/habits/${id}/complete`);
+    setHabits([...habits.filter(({ id }) => id !== data.id), data]);
+  }
+
+  async function incompleteHabit(id: string) {
+    const { data } = await axios.put<Habit>(`api/habits/${id}/incomplete`);
+    setHabits([...habits.filter(({ id }) => id !== data.id), data]);
   }
 
   return (
-    <View style={tailwind('flex-1 justify-center items-center')}>
-      <Text style={tailwind('text-4xl font-bold')}>Habits</Text>
+    <View style={tailwind('flex-1 justify-center bg-blue-100')}>
       <View
-        style={tailwind('my-4 h-1 w-4/5')}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
-      />
-      {habits.map(habit => (
-        <View key={habit.id} style={tailwind('flex-row py-2 px-6 w-full')}>
-          <Switch
-            value={habit.completed}
-            onValueChange={() => toggleHabitCompletion(habit.id)}
-          />
-          <View>
-            <Text style={tailwind('font-bold text-lg')}>{habit.name}</Text>
-            <Text>{JSON.stringify(convertToStreak(habit.streak))}</Text>
+        style={tailwind(
+          'rounded-b-3xl h-1/3 w-full flex justify-center items-center bg-indigo-500'
+        )}
+      >
+        <Text style={tailwind('text-4xl font-bold text-white')}>Habits</Text>
+      </View>
+      <ScrollView style={tailwind('rounded-t-3xl mt-20 pt-10 bg-white')}>
+        {habits.map(habit => (
+          <View
+            key={habit.id}
+            style={tailwind('flex-row py-6 px-12 w-full items-center')}
+          >
+            <Switch
+              style={tailwind('mr-5')}
+              value={habit.completed}
+              onChange={() =>
+                habit.completed
+                  ? incompleteHabit(habit.id)
+                  : completeHabit(habit.id)
+              }
+            />
+            <View>
+              <Text style={tailwind('font-bold text-lg w-2/3')}>
+                {habit.name}
+              </Text>
+              <Text style={tailwind('text-sm')}>{habit.streak}</Text>
+            </View>
           </View>
-        </View>
-      ))}
+        ))}
+      </ScrollView>
     </View>
   );
 }
