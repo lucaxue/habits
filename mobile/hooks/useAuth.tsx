66c5dee81fb29b9926df940ef7d05c19d51202ea
Luca Xue <lxue02@gmail.com>
2021-11-06 17:20:49 +0000

    Extract auth into useAuth hook

---
+++ mobile/hooks/useAuth.tsx
@@ -0,0 +1,97 @@
+import axios from 'axios';
+import React, { useState, useContext, createContext } from 'react';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.withCredentials = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+interface Auth {
+  user: {} | null;
+  authenticated: boolean;
+  register: (
+    name: string,
+    email: string,
+    password: string,
+    passwordConfirmation: string,
+    deviceName: string
+  ) => Promise<void> | void;
+  login: (
+    email: string,
+    password: string,
+    deviceName: string
+  ) => Promise<void> | void;
+  logout: () => Promise<void> | void;
+}
+
+const AuthContext = createContext<Auth>({
+  user: null,
+  authenticated: false,
+  register: () => {},
+  login: () => {},
+  logout: () => {},
+});
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const auth = useProvideAuth();
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
+
+interface AuthenticatedResponse {
+  user: {};
+  token: string;
+}
+
+function useProvideAuth(): Auth {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState<{} | null>(null);
+
+  async function register(
+    name: string,
+    email: string,
+    password: string,
+    passwordConfirmation: string,
+    deviceName: string
+  ) {
+    const { data } = await axios.post<AuthenticatedResponse>('api/register', {
+      name,
+      email,
+      password,
+      password_confirmation: passwordConfirmation,
+      device_name: deviceName,
+    });
+
+    setUser(data.user);
+    axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+    setAuthenticated(true);
+  }
+
+  async function login(email: string, password: string, deviceName: string) {
+    const { data } = await axios.post<AuthenticatedResponse>('api/login', {
+      email,
+      password,
+      device_name: deviceName,
+    });
+
+    setUser(data.user);
+    axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+    setAuthenticated(true);
+  }
+
+  async function logout() {
+    await axios.delete('api/logout');
+
+    setUser(null);
+    axios.defaults.headers.common['Authorization'] = '';
+    setAuthenticated(false);
+  }
+
+  return {
+    user,
+    authenticated,
+    register,
+    login,
+    logout,
+  };
+}
