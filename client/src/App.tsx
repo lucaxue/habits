fe6512854104093414fcd3394ebbbc7cc184b65e
Luca Xue <lxue02@gmail.com>
2021-11-07 11:53:16 +0000

    Add useAuth to react client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useAuth } from './hooks/useAuth';
 
-function App() {
+export const App: React.FC = () => {
+  const [email, setEmail] = useState('john@example.com');
+  const [password, setPassword] = useState('password');
+  const { login, user } = useAuth();
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p className='text-red-500'>If this is red, tailwind is working</p>
-      </header>
+    <div className='h-screen flex-column pt-24 px-10'>
+      <form
+        className='grid'
+        onSubmit={async e => {
+          e.preventDefault();
+          await login(email, password, 'mobile');
+          console.log(user);
+        }}
+      >
+        <input
+          className='border-2 mb-2'
+          type='email'
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
+        <input
+          className='border-2 mb-2'
+          type='password'
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button type='submit'>Login</button>
+      </form>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
-}
-
-export default App;
+};
