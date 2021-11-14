a270fcfb0c3ca6de6b3f90dabb82c7730444518c
Luca Xue <lxue02@gmail.com>
2021-11-14 14:50:24 +0000

    Split up login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Habits } from './components/Habits';
+import { Login } from './components/Login';
 import { useAuth } from './hooks/useAuth';
 
 export const App: React.FC = () => {
-  const [email, setEmail] = useState('john@example.com');
-  const [password, setPassword] = useState('password');
-  const { login, user, authenticated } = useAuth();
+  const { user, authenticated } = useAuth();
 
   return (
-    <div className='h-screen flex-column pt-24 px-10'>
-      {authenticated ? (
-        <Habits />
-      ) : (
-        <form
-          className='grid'
-          onSubmit={async e => {
-            e.preventDefault();
-            await login(email, password, 'mobile');
-            console.log(user);
-          }}
-        >
-          <input
-            className='border-2 mb-2'
-            type='email'
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-          />
-          <input
-            className='border-2 mb-2'
-            type='password'
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <button type='submit'>Login</button>
-        </form>
-      )}
+    <div className='h-screen'>
+      {authenticated ? <Habits /> : <Login />}
       <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
   );
