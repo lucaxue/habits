3aa202331bfca788337a8e755d289be36fd72cdc
Luca Xue <lxue02@gmail.com>
2021-11-20 16:42:07 +0000

    Style navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,15 +20,47 @@ export const App: React.FC = () => {
         <Route path='/tracking' element={<Tracking />} />
         <Route path='/habits' element={<Habits />} />
         <Route path='/add' element={<Start />} />
-        <Route path='/profile' element={<Profile/>} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
 
-      <nav className='fixed bottom-0 w-full bg-white shadow-2xl rounded-t-3xl'>
-        <ul className='flex justify-between w-full p-4'>
-          <li><Link to='/tracking'>Tracking</Link></li>
-          <li><Link to='/habits'>Habits</Link></li>
-          <li><Link to='/add'>Add</Link></li>
-          <li><Link to='/profile'>Profile</Link></li>
+      <nav className='fixed bottom-0 w-full'>
+        <Link
+          className='absolute z-10 px-4 pt-4 pb-3 transform -translate-x-1/2 bg-indigo-500 rounded-full -top-6 left-1/2'
+          to='/add'
+        >
+          <span className='p-0 m-0 text-white fill-current material-icons'>
+            add_task
+          </span>
+        </Link>
+        <ul className='relative flex justify-between w-full p-6 bg-white shadow-2xl rounded-t-3xl'>
+          <li>
+            <Link to='/tracking'>
+              <span className='text-indigo-600 fill-current material-icons'>
+                fact_check
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to='/habits'>
+              <span className='text-indigo-600 fill-current material-icons'>
+                stacked_bar_chart
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to='/profile'>
+              <span className='text-indigo-600 fill-current material-icons'>
+                face
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to='/profile'>
+              <span className='text-indigo-600 fill-current material-icons'>
+                face
+              </span>
+            </Link>
+          </li>
         </ul>
       </nav>
     </Router>
