2bb37d84db1ea2388ffc8761462bd66614dfbe98
Luca Xue <lxue02@gmail.com>
2021-11-21 16:40:55 +0000

    Style login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -7,26 +7,36 @@ export const Login: React.FC = () => {
   const { login } = useAuth();
 
   return (
-    <form
-      className='grid'
-      onSubmit={async e => {
-        e.preventDefault();
-        await login(email, password, 'mobile');
-      }}
-    >
-      <input
-        className='mb-2 border-2'
-        type='email'
-        value={email}
-        onChange={({ target }) => setEmail(target.value)}
-      />
-      <input
-        className='mb-2 border-2'
-        type='password'
-        value={password}
-        onChange={({ target }) => setPassword(target.value)}
-      />
-      <button type='submit'>Login</button>
-    </form>
+    <div className='min-h-screen bg-gray-50'>
+      <div className='fixed bottom-0 w-full overflow-auto shadow-2xl h-1/2 rounded-t-3xl bg-gradient-to-b from-white via-white to-gray-50'>
+        <form
+          className='grid p-8'
+          onSubmit={async e => {
+            e.preventDefault();
+            await login(email, password, 'mobile');
+          }}
+        >
+          <h1 className='mx-auto mb-8 text-gray-700'>Login with email</h1>
+          <input
+            className='h-12 mb-2 text-gray-700 border border-gray-300 rounded-lg shadow'
+            type='email'
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+          />
+          <input
+            className='h-12 mb-2 text-gray-700 border border-gray-300 rounded-lg shadow'
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <button
+            className='py-3 mt-4 font-semibold text-white bg-indigo-500 rounded shadow'
+            type='submit'
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
