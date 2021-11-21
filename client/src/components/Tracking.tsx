9ed0364f5c3259af655bb3811778b224e59d847e
Luca Xue <lxue02@gmail.com>
2021-11-21 17:21:43 +0000

    Add incomplete habits count on tracking page

--- client/src/components/Tracking.tsx
+++ client/src/components/Tracking.tsx
@@ -28,9 +28,17 @@ export const Tracking: React.FC = () => {
   return (
     <div className='min-h-screen bg-gray-50'>
       <div className='fixed top-0 flex items-center w-full px-8 bg-gradient-to-br from-indigo-400 to-indigo-600 h-1/3 rounded-b-3xl'>
-        <h1 className='text-4xl text-white'>
-          Hi, <strong>{user?.name}</strong>
-        </h1>
+        <span className='transform translate-y-1/3'>
+          <h1 className='text-4xl text-white'>
+            Hi, <strong>{user?.name}</strong>
+          </h1>
+          <p className='mt-2 text-white'>
+            {habits.every(h => h.completed)
+              ? 'You have completed all of your habits!'
+              : `You have ${habits.filter(h => !h.completed).length} habits to
+            complete.`}
+          </p>
+        </span>
       </div>
       <div className='fixed bottom-0 w-full py-8 overflow-auto shadow-2xl pb-28 h-1/2 rounded-t-3xl bg-gradient-to-b from-white via-white to-gray-50'>
         {habits
