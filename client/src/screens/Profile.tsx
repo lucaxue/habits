538124760544ca1cd0eb0aa6e60f5adc84dc3211
Luca Xue <lxue02@gmail.com>
2021-11-28 12:02:40 +0000

    Update profile page

--- client/src/screens/Profile.tsx
+++ client/src/screens/Profile.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useAuth } from '../hooks/useAuth';
+import { durationFrom } from '../utils/helpers';
 
 export const Profile: React.FC = () => {
   const { user, logout } = useAuth();
+  const memberDuration = durationFrom(new Date(user?.created_at ?? new Date()));
 
   return (
-    <div className='min-h-screen bg-gray-50'>
+    <div className='min-h-screen bg-white'>
       <div className='fixed top-0 flex items-end justify-center w-full px-8 bg-gradient-to-br from-indigo-400 to-indigo-600 h-1/3 rounded-b-3xl'>
         <img
           className='absolute p-6 bg-white rounded-full shadow-xl -bottom-1/3 h-44'
@@ -14,18 +16,19 @@ export const Profile: React.FC = () => {
         />
       </div>
 
-      <div className='fixed bottom-0 grid w-full gap-2 p-8 pb-24 overflow-auto shadow-2xl h-1/2 rounded-t-3xl bg-gradient-to-b from-white via-white to-gray-50'>
-        <pre className='px-4 overflow-auto text-xs'>
-          {JSON.stringify(user, null, 2)}
-        </pre>
-        <div className='w-full'>
-          <button
-            className='w-full py-3 mt-4 font-semibold text-white bg-indigo-500 rounded shadow'
-            onClick={async e => logout()}
-          >
-            Logout
-          </button>
+      <div className='fixed bottom-0 flex-col w-full px-8 pb-32 h-1/2 bg-gradient-to-b from-white via-white to-gray-50'>
+        <div className='text-center'>
+          <h1 className='text-3xl font-semibold text-gray-700'>{user?.name}</h1>
+          <p className='text-gray-500'>
+            Joined {memberDuration.amount} {memberDuration.unit} ago
+          </p>
         </div>
+        <button
+          className='w-full py-3 mt-8 font-semibold text-white bg-indigo-500 rounded shadow'
+          onClick={async e => logout()}
+        >
+          Logout
+        </button>
       </div>
     </div>
   );
